c9b4b7e563e5773b9fe03dbacea0b1a3f16ef530
taronklm <taroanklam@gmail.com>
2024-12-06 15:32:41 +0100

    feat: changing route

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -5,7 +5,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'POST') {
     console.log(req.body.message)
     try {
-      const response = await axios.post('http://localhost:8000/generate', { input_text: req.body.message });
+      const response = await axios.post('http://localhost:8000/chatbot/', { input_text: req.body.message });
       console.log("RESPONSE DATA: ", response.data.generated_text)
       res.status(response.status).json(response.data.generated_text);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
