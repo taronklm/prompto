b6559289640fad729e15307db30e76e6c5a9b545
taronklm <taroanklam@gmail.com>
2024-12-07 15:06:28 +0100

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,11 @@
-"use client"
+import React from "react"
 
-import { useState } from 'react';
-import axios from 'axios';
-
-const Chat = () => {
-  const [message, setMessage] = useState<string>('');
-  const [responses, setResponses] = useState<{ user: string; bot: string }[]>([]);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (message.trim() === "") return;
-
-    const res = await axios.post('/api/chat', { message: message });
-    console.log("RESPONSE: ", res.data.response.generated_text)
-    setResponses((prev) => [...prev, { user: message, bot: res.data.response.generated_text }]);
-    setMessage('');
-  };
-
-  return (
+const Home: React.FC = () => {
+  return(
     <div>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          value={message} 
-          onChange={(e) => setMessage(e.target.value)} 
-          placeholder="Type your message" 
-        />
-        <button type="submit">Send</button>
-      </form>
-      <div>
-        <div>
-          <p> Bot: How can I help you? </p>
-        </div>
-          {responses.map((r, index) => (
-            <div key={index}>
-              <p>User: {r.user}</p>
-              <p>Bot: {r.bot}</p>
-            </div>
-          ))
-        }
-      </div>
+      Home
     </div>
-  );
-};
+  )
+}
 
-export default Chat;
+export default Home
