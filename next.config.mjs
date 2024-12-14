68833ccf2d8701529f5e968f501eb07b3d2cbce8
taronklm <taroanklam@gmail.com>
2024-12-14 18:05:55 +0100

    feat: remove redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return[
-            {
-                source: '/',
-                destination: '/assistant',
-                permanent: true,
-            }
-        ]
-    }
 };
 
 export default nextConfig;
